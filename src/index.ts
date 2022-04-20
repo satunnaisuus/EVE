63ef7df3032c4dd85e51366bb37a32d61e384aa6
satunnaisuus <satunnaisuus@yandex.ru>
2022-04-21 00:19:21 +0300

    + canvas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const canvasContainer = document.getElementById("canvas-container");
+
+function fitCanvas(canvas: HTMLCanvasElement, container: HTMLElement) {
+    canvas.setAttribute("width", container.clientWidth.toString());
+    canvas.setAttribute("height", container.clientHeight.toString());
+}
+
+setInterval(() => {
+    fitCanvas(canvas, canvasContainer);
+}, 1000);
